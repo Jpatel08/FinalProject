d786c2733da4c9da2a326d653ae9efda89b4cf9b
Jpatel08 <jaypatel515121@gmail.com>
2023-05-04 15:18:36 -0500

    added form validation

--- FinalProjFrontend/src/App.js
+++ FinalProjFrontend/src/App.js
@@ -137,7 +137,63 @@ const ShoppingCart = ({ cartItems, onRemoveFromCart, total }) => {
   );
 };
 
-const Cart = ({ cartItems, onRemoveFromCart, total }) => {
+
+const Cart = ({ cartItems, onRemoveFromCart, total, onViewChange, handleFormChange}) => {
+
+  // const addInfo = () => {
+  //   console.log(userInfo);
+  //   setUserInfo({...userInfo, fullName: document.getElementById('inputName').value});
+  //   console.log(userInfo);
+  //   console.log(document.getElementById('inputName').value);
+  //   setUserInfo({...userInfo, email: document.getElementById('inputEmail4').value});
+  //   console.log(document.getElementById('inputEmail4').value);
+  //   setUserInfo({ cardInfo: document.getElementById('inputCard').value});
+  //   console.log(document.getElementById('inputCard').value);
+  //   console.log(userInfo);
+  //   setUserInfo({...userInfo, address: document.getElementById('inputAddress').value + document.getElementById('inputAddress2').value});
+  //   console.log(document.getElementById('inputAddress').value + document.getElementById('inputAddress2').value)
+    
+  //   console.log('yo');
+  //   console.log(userInfo);
+  // }
+
+  // Form validation stuff
+  let validate = function () {
+    let val = true;
+    let email = document.getElementById('inputEmail4')
+    let name = document.getElementById('inputName')
+    let card = document.getElementById('inputCard')
+    let form = document.getElementById('form')
+    if (!email.value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9] {1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )) {
+      email.setAttribute("class", "form-control is-invalid");
+      val = false;
+    }
+    else {
+      email.setAttribute("class", "form-control is-valid");
+    }
+    if (name.value.length == 0) {
+      name.setAttribute("class", "form-control is-invalid")
+      val = false
+    }
+    else {
+      name.setAttribute("class", "form-control is-valid");
+    }
+    if (!card.value.match(/^[0-9]{4}\-[0-9]{4}\-[0-9]{4}\-[0-9]{4}$/)) {
+      card.setAttribute("class", "form-control is-invalid")
+      val = false
+    }
+    else {
+      card.setAttribute("class", "form-control is-valid");
+    }
+    if (val) {
+      form.classList.add("collapse")
+      alert('You have made an order!', 'success')
+    }
+    return val;
+  }
+
+
   return (
     <div className="container mt-3">
       <h1>Shopping Cart</h1>
@@ -167,21 +223,165 @@ const Cart = ({ cartItems, onRemoveFromCart, total }) => {
           </table>
           <div className="total-price">Total: ${total}</div>
           <div>
-            <form>
+            <form id="form">
 
-            </form>
+            <><h1>Payment Information</h1><div id="liveAlertPlaceholder"></div><form class="row g-3" id="checkout-form">
+      <div class="col-md-6">
+        <label for="inputName" class="form-label">
+          Full Name
+        </label>
+        <input type="text" class="form-control" id="inputName" onChange={handleFormChange}/>
+        <div class="valid-feedback">Looks good!</div>
+        <div class="invalid-feedback">
+          Must be like, "John Doe"
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <label for="inputEmail4" class="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          class="form-control"
+          id="inputEmail4"
+          onChange={handleFormChange} />
+        <div class="valid-feedback">Looks good!</div>
+        <div class="invalid-feedback">
+          Must be like, "abc@xyz.efg"
+        </div>
+      </div>
+
+      <div class="col-12">
+        <label for="inputCard" class="form-label">
+          Card
+        </label>
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="basic-addon1">
+            <i class="bi-credit-card-fill"></i>
+          </span>
+          <input
+            type="text"
+            id="inputCard"
+            onChange={handleFormChange}
+            class="form-control"
+            placeholder="XXXX-XXXX-XXXX-XXXX"
+            aria-label="Username"
+            aria-describedby="basic-addon1" />
+          <div class="valid-feedback">Looks good!</div>
+          <div class="invalid-feedback">
+            Must be like, "7777-7777-7777-7777"
           </div>
-        </>
-      ) : (
+        </div>
+      </div>
+
+      <div class="col-12">
+        <label for="inputAddress" class="form-label">
+          Address
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          id="inputAddress"
+          onChange={handleFormChange}
+          placeholder="1234 Main St" />
+      </div>
+      <div class="col-12">
+        <label for="inputAddress2" class="form-label">
+          Address 2
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          id="inputAddress2"
+          placeholder="Apartment, studio, or floor" />
+      </div>
+      <div class="col-md-6">
+        <label for="inputCity" class="form-label">
+          City
+        </label>
+        <input type="text" class="form-control" id="inputCity" />
+      </div>
+      <div class="col-md-6">
+        <label for="inputCity" class="form-label">
+          State
+        </label>
+        <input type="text" class="form-control" id="inputCity" />
+      </div>
+      <div class="col-md-2">
+        <label for="inputZip" class="form-label">
+          Zip
+        </label>
+        <input type="text" class="form-control" id="inputZip" onChange={handleFormChange} />
+      </div>
+      <div class="col-12"></div>
+      <div class="col-12">
+        <button type="submit" class="btn btn-success" onClick={() => {if(validate())onViewChange('confirmation')}}>
+          {" "}
+          <i class="bi-bag-check"></i> Order
+        </button>
+      </div>
+    </form></>
+              
+    </form>
+    </div>
+     </>
+     ) : (
         <p>Your cart is empty</p>
       )}
     </div>
   );
 };
 
+
+const Confimation = ({cartItems, total, userInfo, onViewChange}) => {
+  return(
+    <div className="container mt-3">
+      <h1>Confimation</h1>
+      <>
+      <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Product</th>
+          <th scope="col">Price</th>
+          <th scope="col">Quantity</th>
+          <th scope="col">Image</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      {cartItems.map((item) => (
+        <tr key={item.id}>
+          <td>{item.title}</td>
+          <td>${item.price}</td>
+          <td>{item.quantity}</td>
+          <td><img src={item.image} alt={item.title} className="img-thumbnail" width="115px" height="115px"></img></td>
+        </tr>
+      ))}
+      </table>
+      <div className="total-price"><b>Total:</b> ${total}</div>
+      <div>
+        <h4>User Info</h4>
+        <>
+        <p>Full Name:{userInfo.inputName} <br></br> Email Address: {userInfo.inputEmail4} <br></br>Card Info: {userInfo.inputCard} <br></br>Given Address: {userInfo.inputAddress}</p></>
+        <a href="/"><button type="submit" class="btn btn-success" onClick={() => {onViewChange('products')}}>
+          {" "}
+          <i class="bi-bag-check"></i> Return
+        </button></a>
+      </div>
+      </>
+    </div>
+  )
+}
 const App = () => {
   const [view, setView] = useState('products');
   const [cartItems, setCartItems] = useState([]);
+  const [userInfo, setUserInfo] = useState({})
+
+
+  const handleUserChange = (event) => {
+    setUserInfo({ ...userInfo, [event.target.id]:event.target.value});
+    console.log(userInfo);
+  };
 
   const handleViewChange = (newView) => {
     setView(newView);
@@ -208,13 +408,14 @@ const App = () => {
     return total.toFixed(2);
   };
   
-  
+  console.log(userInfo);
   return (
     <div class="pageBG">
       <Navbar onViewChange={handleViewChange} cartItems={cartItems} />
       <div className="container">
         {view === 'products' && <ProductGrid onAddToCart={handleAddToCart} />}
-        {view === 'cart' && <Cart cartItems={cartItems} onRemoveFromCart={handleRemoveFromCart} total={calculateTotal()} />}
+        {view === 'cart' && <Cart cartItems={cartItems} onRemoveFromCart={handleRemoveFromCart} total={calculateTotal()} onViewChange={handleViewChange} handleFormChange={handleUserChange} />}
+        {view === 'confirmation' && <Confimation cartItems={cartItems} total={calculateTotal()} userInfo={userInfo} onViewChange={handleViewChange} />} 
       </div>
     </div>
   );
@@ -222,5 +423,3 @@ const App = () => {
 
 
 export default App;
-
-
