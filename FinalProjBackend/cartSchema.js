07c462d3e987f2e51029d1d9616b0479076aab9c
Jpatel08 <jaypatel515121@gmail.com>
2023-05-05 19:19:36 -0500

    Created schema for different data types

---
+++ FinalProjBackend/cartSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const ReactFormDataSchema = new mongoose.Schema(
+  {
+    _id: { type: Number },
+    title: { type: String },
+    price: { type: Number },
+    description: { type: String },
+    category: { type: String },
+    image: { type: String },
+    quantity: {type: Number},
+  },
+  { collection: "Cart" }
+);
+
+const Product = mongoose.model('Product', ReactFormDataSchema);
+
+module.exports = {
+  cartItem,
+};
